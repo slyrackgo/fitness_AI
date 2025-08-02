ba2fd706cd29c4e71dec4afa09e116035b2cdb01
Atai Ibragimov <aibragimov2003@bk.ru>
2025-08-02 23:27:51 +0200

    Session 2: Recommendation service created.

---
+++ AI_Service/AI_Service/src/test/java/com/fitness/AI_Service/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.AI_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
