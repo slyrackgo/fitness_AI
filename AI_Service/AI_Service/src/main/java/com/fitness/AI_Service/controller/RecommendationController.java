db4f2df7279d3f857b908215a477a0019b1dafce
Atai Ibragimov <aibragimov2003@bk.ru>
2025-08-02 23:27:05 +0200

    Session 2: Recommendation service created.

---
+++ AI_Service/AI_Service/src/main/java/com/fitness/AI_Service/controller/RecommendationController.java
@@ -0,0 +1,30 @@
+package com.fitness.AI_Service.controller;
+
+import com.fitness.AI_Service.model.Recommendation;
+import com.fitness.AI_Service.service.RecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId){
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId){
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
+}
---
+++ AI_Service/AI_Service/src/main/java/com/fitness/AI_Service/service/RecommendationService.java
@@ -0,0 +1,23 @@
+package com.fitness.AI_Service.service;
+
+import com.fitness.AI_Service.model.Recommendation;
+import com.fitness.AI_Service.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("Now recommendation found for this activity. " + activityId));
+    }
+}
