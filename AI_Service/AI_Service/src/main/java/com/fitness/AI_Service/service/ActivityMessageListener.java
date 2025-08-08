9458113d70078a15522a0150610fb30dadf3e1f9
Atai Ibragimov <aibragimov2003@bk.ru>
2025-08-08 20:47:43 +0200

    Session 5: AI service fixed.

--- AI_Service/AI_Service/src/main/java/com/fitness/AI_Service/service/ActivityMessageListener.java
+++ AI_Service/AI_Service/src/main/java/com/fitness/AI_Service/service/ActivityMessageListener.java
@@ -1,8 +1,6 @@
 package com.fitness.AI_Service.service;
 
 import com.fitness.AI_Service.model.Activity;
-import com.fitness.AI_Service.model.Recommendation;
-import com.fitness.AI_Service.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,13 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
     private final ActivityAIService aiService;
-    private final RecommendationRepository recommendationRepository;
-
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("Received activity for processing: {}", activity.getId());
-//        log.info("Generated recommendation: {}", aiService.generateRecommendation(activity));
-        Recommendation recommendation = aiService.generateRecommendation(activity);
-        recommendationRepository.save(recommendation);
+        log.info("Generated recommendation: {}", aiService.generateRecommendation(activity));
+
     }
 }
